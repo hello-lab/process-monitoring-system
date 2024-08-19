e14f4f1f676d866e7ed6742b65df333bb56e24be
ezio <58252733+hello-lab@users.noreply.github.com>
2024-08-19 23:58:30 +0530

    Add files via upload

--- monitored-pc/main.py
+++ monitored-pc/main.py
@@ -30,13 +30,14 @@ whitelist = eval(nstr)
 while True:
     # Run the PowerShell command to get the list of opened applications
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-
+   
     # Iterate over the output of the command
     for line in proc.stdout:
         if line.rstrip():
+            print(line.rstrip())
             # Decode the binary string and remove trailing characters
             x = line.decode().rstrip()
-
+            
             # Check if the opened application is in the whitelist
             if x in whitelist:
                 continue
@@ -45,13 +46,13 @@ while True:
                 dictToSend = {'APPS-OPENNED': x, 'Computer Name': cid}
                 res = requests.post(ip + "/login/", json=dictToSend)
                 print('response from server:', res.text)
-
+               
                 # Check if the request was successful
                 if res:
                     dictFromServer = res.json()
 
                 # Make a GET request to trigger some action on the server (URL might need adjustment)
-                requests.get('http://127.0.0.1:8080/')
+               
 
     # Wait for 5 seconds before repeating the process
     time.sleep(5)
